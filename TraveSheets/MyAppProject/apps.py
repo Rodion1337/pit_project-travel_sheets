f5c6b476207f72f99058afd54db7c669a82b9eba
Radik1337Blr <radikmotocross@gmail.com>
2023-05-09 23:25:42 +0300

    сделал основной функционал пересчета данных

--- TraveSheets/MyAppProject/apps.py
+++ TraveSheets/MyAppProject/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MyappprojectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MyAppProject'
+
+    def ready(self):
+        from MyAppProject import signals
