a94169abf91ab5efd1f5b8a419c223872c38ef97
Rodion Kotaliov <rodionkotaliov@gmail.com>
2025-03-29 17:49:07 +0300

    clear main branch

--- TraveSheets/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-
-
-class User(AbstractUser):
-    icon_user = models.ImageField(verbose_name = 'Иконка пользователя', upload_to = 'icon_user', height_field=None, width_field=None, max_length=None, blank=True, null=True, default='icon_user/default-user-profile-picture.jpg',)
